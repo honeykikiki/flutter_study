0b60fb5ee615dded1169bbcb53d848814b26adc5
honeykikiki <tjdwls75666@naver.com>
2022-04-13 22:53:10 +0900

    Drawer 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,111 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false, // flutter 빨간띠 없애주는 코드
-      title: 'BBANRO',
-      home: Grade(),
+    return MaterialApp(
+      title: 'Appbar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red
+      ),
+      home: MyPage(),
     );
   }
 }
 
-
-class Grade extends StatelessWidget {
-  const Grade({ Key? key }) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text('BBANTO'),
-        backgroundColor: Colors.amber[600],
-        centerTitle: true,
+        backgroundColor: Colors.red,
+        title: Text('AppBar Icon Menu'),
         elevation: 0.0,
+        // leading: IconButton(
+        //   icon: Icon(Icons.menu),
+        //   onPressed: () { 
+        //     print('menu button');
+        //   },
+        // ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () { 
+              print('shopingCart');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () { 
+              print('search');
+            },
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/kikiki.png'),
-                radius: 60.0,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('honey'),
+              accountEmail: Text('honey.kikiki'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/honey.png'),
+                backgroundColor: Colors.white
               ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            const Text('NAME', style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text('BBANTO ', style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            const Text('BANNTO POWER LEVEL', style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text('14 ', style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.child_care_outlined),
-                SizedBox(width: 10.0),
-                Text('using lightsaver', style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),)
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/honey.png'),
+                  backgroundColor: Colors.white
+                ),
+                // CircleAvatar(
+                //   // backgroundImage: AssetImage('assets/honey.png'),
+                //   backgroundColor: Colors.black
+                // )
               ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.child_care_outlined),
-                SizedBox(width: 10.0),
-                Text('face here tatto', style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),)
-              ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.child_care_outlined),
-                SizedBox(width: 10.0),
-                Text('fire flames', style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),)
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: const AssetImage('assets/honey.png'),
-                radius: 40.0,
-                backgroundColor:Colors.amber[800],
+              onDetailsPressed: () {
+                print('arrow in clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0)
+                )
               ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.grey,),
+              title: Text('Home'),
+              onTap: () {
+                print('Home');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings, color: Colors.grey,),
+              title: Text('settings'),
+              onTap: () {
+                print('settings');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey,),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A');
+              },
+              trailing: Icon(Icons.add),
             )
-          ]
+          ],
         ),
       ),
     );
