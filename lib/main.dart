4249a0e51bd8ce6ad7ce7a14ac8773af27e532e6
honeykikiki <tjdwls75666@naver.com>
2022-04-15 23:59:51 +0900

    Toast

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,83 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // 데스크탑, 앱 등에 고루 UI를 적용할 수 있게 해주는 구글이 제공해주는 패키
 import 'package:fluttertoast/fluttertoast.dart';
-main() => runApp(MyApp());
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Appbar',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.red
-      ),
-      home: MyPage(),
+      // 실질적으로 모든 앱을 감싸고 있다.
+      title: 'Appbar', // 앱을 총칭하는 이름 -> 스마트 폰 앱에서 최근 사용한 앱 보여줄 때의 이름
+      theme:
+          ThemeData(primarySwatch: Colors.red // 특정색의 응용들을 기본 색상으로 지정해서 사용하겠
+              ),
+      home: MyPage(), // home은 앱이 실행될 때 가장먼저 보여주는 페이
     );
   }
 }
 
 class MyPage extends StatelessWidget {
-  const MyPage({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('snack bar'),
+        title: Text('Toast Bar'),
         centerTitle: true,
       ),
-      body: MyToast(),
-    );
-  }
-}
-
-class MyToast extends StatelessWidget {
-  const MyToast({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {},
-        child: Text('Toast'),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            print('TextButton is clicked');
+            flutterToast(); // 함수 호출
+          },
+          child: Text('Toast'),
+          //color: Colors.blue
+        ),
       ),
-      
     );
   }
 }
+
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: "This is Center Short Toast", // 메시지 내용
+      toastLength: Toast.LENGTH_SHORT, // 메시지 시간 - 안드로이드
+      // gravity: ToastGravity.BOTTOM, // 메시지 위치
+      timeInSecForIosWeb: 1, // 메시지 시간 - iOS 및 웹
+      backgroundColor: Colors.red, // 배경
+      textColor: Colors.black, // 글자
+      fontSize: 26.0,
+      webBgColor:'red',
+      webPosition: 'center' // 위치 조절
+    ); // 글자 크기
+}
+
+// class MyToast extends StatelessWidget {
+//   const MyToast({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextButton(
+//       onPressed: () {
+//         flutterToast();
+//       },
+//       child: Container(
+//         padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+//         decoration: const BoxDecoration(
+//           color: Colors.blue,
+//         ),
+//         child: const Text('Toast', 
+//           style: TextStyle(
+//             color: Colors.red,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 
 
 // class MySnackBar extends StatelessWidget {
